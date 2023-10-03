1e4b6bf988a4e299542f10a9514289269cefabe3
RapaxDB <23dimon19@gmail.com>
2023-10-04 01:59:38 +0500

    private field UserController

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/users")
 public class UserController {
-    UserService service;
+    private UserService service;
     @Autowired
     public UserController (UserService service) {
         this.service = service;
